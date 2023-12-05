6e58290d1f549fcadf0b6eeafc6836a02312bc95
shafeerashafu <shafeerazahur3@gmail.com>
2023-12-06 00:19:14 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+1.API endpoint, which will create a text file in a particular folder.
+1)content of the file should be current timestamp
+2)the filename should be current date-time.txt
+
+API endpoint that I used: '/createFile'
+
+
+2.API endpoint to retrieve all the textfiles in a particular created folder.
+
+API endpoint that I used: '/getAllFiles'
---
+++ index.js
@@ -0,0 +1,61 @@
+
+
+
+
+
+
+
+
+// const fs = require('fs');
+
+// fs.mkdir("My-Folder", () => {
+//     console.log("Folder Created Successfullys");
+//   });
+
+
+
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = 5000;
+
+// 1. Endpoint to create a file
+app.post('/createFile', (req, res) => {
+  const folderPath = 'Sample-folder';
+  const currentDate = new Date().toISOString().replace(/:/g, '-');
+  const fileName = `${currentDate}.txt`;
+  const filePath = path.join(folderPath, fileName);
+  const content = `${Date.now()}`; 
+
+  try {
+    fs.writeFileSync(filePath, content);
+    res.status(200).send(`File ${fileName} created in ${folderPath} with content: ${content}`);
+  } catch (err) {
+    res.status(500).send('Error creating file');
+  }
+});
+
+
+// 2. Endpoint to retrieve all the files in a particular folder
+
+app.get('/getAllFiles',(req,res)=>{
+    const folderPath='My-Folder';
+    try {
+        const files = fs.readdirSync(folderPath);
+        res.status(200).json({ files });
+      } catch (err) {
+        res.status(500).send('Error retrieving files');
+      }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+
+
+
+
