3cee5fca569ab6c94ee7eb30a41d4fadd132719a
shafeerashafu <shafeerazahur3@gmail.com>
2023-12-06 12:41:35 +0530

    Last commit

--- index.js
+++ index.js
@@ -4,13 +4,13 @@
 
 
 
+//Creating a folder.
 
+const fs = require('fs');
 
-// const fs = require('fs');
-
-// fs.mkdir("My-Folder", () => {
-//     console.log("Folder Created Successfullys");
-//   });
+fs.mkdir("New-Folder", () => {
+    console.log("Folder Created Successfullys");
+  });
 
 
 
@@ -21,24 +21,11 @@ const path = require('path');
 const app = express();
 const port = 5000;
 
-// 1. Endpoint to create a file
-// app.post('/createFile', (req, res) => {
-//   const folderPath = 'Sample-folder';
-//   const currentDate = new Date().toISOString().replace(/:/g, '-').replace(/\..+/, '');
-//   const fileName = `${currentDate}.txt`;
-//   const filePath = path.join(folderPath, fileName);
-//   const content = `${Date.now()}`; 
+// 1. Endpoint to create a file in a particular folder
 
-//   try {
-//     fs.writeFileSync(filePath, content);
-//     res.status(200).send(`File ${fileName} created in ${folderPath} with content: ${content}`);
-//   } catch (err) {
-//     res.status(500).send('Error creating file');
-//   }
-// });
 
 app.post('/createfile', (req, res) => {
-  const folderPath = 'Sample-Folder'; 
+  const folderPath = 'New-Folder'; 
   const currentDate = new Date();
   const formattedDate = currentDate.toISOString(); 
   const fileName = `${formattedDate}.txt`;
@@ -57,7 +44,7 @@ app.post('/createfile', (req, res) => {
 // 2. Endpoint to retrieve all the files in a particular folder
 
 app.get('/getAllFiles',(req,res)=>{
-    const folderPath='My-Folder';
+    const folderPath='New-Folder';
     try {
         const files = fs.readdirSync(folderPath);
         res.status(200).json({ files });
